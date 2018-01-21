3fe119fd3a063d3d1d562aed5a1d4b06a5261d7d
Borivoje Skrbina <borivoje.s@gmail.com>
2018-01-21 15:02:35 +0100

    skoro gotova naslovna strana

--- med/public_html/js/main.js
+++ med/public_html/js/main.js
@@ -1,31 +1,117 @@
-$(document).ready(function(){
-  
-    
-    
-    
-    
+$(document).ready(function () {
+
+    if ($('.owl-carousel').length > 0) {
+        $('.authors-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+        $('.sponsors-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+
+    }
+
+
+
+
+    if ($('.calendar-container').length > 0) {
+        $("#container").simpleCalendar({
+            months: ['januar', 'februar', 'mart', 'april', 'maj', 'jun', 'jul', 'avgust', 'septembar', 'oktobar', 'novembar', 'decembar'], //string of months starting from january
+            days: ['ne', 'po', 'ut', 'sr', 'če', 'pe', 'su'], //string of days starting from sunday
+            minDate: "YYYY-MM-DD", // minimum date
+            maxDate: "YYYY-MM-DD", // maximum date
+            insertEvent: false, // can insert events
+            displayEvent: false, // display existing event
+            fixedStartDay: true, // Week begin always by monday
+            event: [], //List of event
+            insertCallback: function () {} // Callback when an event is added to the calendar
+        });
+
+        $("#container a").click(function (e) {
+            e.preventDefault();
+
+        });
+    }
+
+
+
+
+    $('.example-fontawesome-o').each(function () {
+        var currentRating = $(this).data('current-rating');
+
+        $(this).find('.current-rating')
+                .find('span')
+                .html(currentRating);
+
+        $(this).find('.clear-rating').on('click', function (event) {
+            event.preventDefault();
+
+            $(this)
+                    .barrating('clear');
+        });
+        $(this).barrating({
+            theme: 'fontawesome-stars-o',
+            showSelectedRating: false,
+            initialRating: currentRating,
+            onSelect: function (value, text) {
+                if (!value) {
+                    $(this)
+                            .barrating('clear');
+                } else {
+                    $(this).find('.current-rating')
+                            .addClass('hidden');
+
+                    $(this).find('.your-rating')
+                            .removeClass('hidden')
+                            .find('span')
+                            .html(value);
+                }
+            },
+            onClear: function (value, text) {
+                $(this).find('.stars-example-fontawesome-o')
+                        .find('.current-rating')
+                        .removeClass('hidden')
+                        .end()
+                        .find('.your-rating')
+                        .addClass('hidden');
+            }
+        });
+    });
+
+
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
